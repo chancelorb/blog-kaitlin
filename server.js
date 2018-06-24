b05d99a4a2797393b58374ce3825555efaaac920
Chance Baars <chance@Chance.home>
2018-06-24 18:16:09 -0400

    created server

---
+++ server.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const authController = require('./controllers/authController');
+
+const Router = require('./routes')
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(authController.receiveToken);
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
+app.use('/', Router);
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+})
